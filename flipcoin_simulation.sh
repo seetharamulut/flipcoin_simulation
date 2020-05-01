6d291948d0b09cc79faf21b4504a69dfaa1aac42
seetharamulu <thumurugoti.seetharamulu@gmail.com>
2020-05-01 13:24:06 +0530

    added flipcoin_simulation.sh

--- flipcoin_simulation.sh
+++ flipcoin_simulation.sh
@@ -1,9 +1,18 @@
-#! /bin/bash/ 
-random=$(( $RANDOM%2 ))
+#! /bin/bash/  
+head_count=0
+tail_count=0
+total_count=50
 
-if [ $random -eq 1 ]
-then 
-    echo "heads is the winner"
-else 
-    echo "tails is the winner"
-fi
+while [ $total_count -gt 0 ]
+do
+   random=$(( $RANDOM%2 ))
+   if [ $random -eq 1 ]
+   then 
+    head_count=$(( $head_count+1 ))
+   else 
+    tail_count=$(( $tail_count+1 ))
+   fi
+ total_count=$(( $total_count-1 ))
+done
+echo "number of times head won : "$head_count
+echo "number of times tails won : "$tail_count
